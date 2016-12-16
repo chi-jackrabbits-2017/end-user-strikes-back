f006d36d6ba44be1804636ffeade91c7f6e8ed8b
Michael  Taylor <email": null>
2016-12-16 12:27:03 -0600

    12:26, best answer button is now working with jquery as well

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,9 +1,9 @@
 
 $(function(){
-  $(".question-body, .question-answer").hide();
+  $(".question-body, .question-answer, .best-answer").hide();
     $(".show-more").on('click', function(event) {
     event.preventDefault();
     console.log(this)
-    $(this).siblings("li").show()
+    $(this).siblings("li, button").show()
   });
 });
