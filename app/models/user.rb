2be8be25512e9ee724dc72f16751c9daf805800f
Tyler Small <email": null>
2016-12-15 19:56:49 -0600

    Add Faker seeds for later testing. Change Comment model to include many votes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
   has_many :answers, foreign_key: :contributor_id
   has_many :votes, foreign_key: :voter_id
   has_many :comments, foreign_key: :commenter_id
+
+  def password
+    @password ||= BCrypt::Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.hashed_password = @password
+  end
 end
