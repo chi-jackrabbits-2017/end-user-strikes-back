650e01db25e14a32688b947cdb5ace6607b51c27
Justin Frederick <email": null>
2016-12-16 11:05:22 -0600

    add get questions/show route, fri, 11:05am

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,6 +1,17 @@
 get '/questions' do
-  @questions = Question.all
+  @questions = Question.order(created_at: :desc)
   erb :'questions/index'
 end
 
+get '/questions/:id' do
+  @question = Question.find_by(id: params[:id])
+
+  if @question
+    erb :'questions/show'
+  else
+    @questions = Question.order(created_at: :desc)
+    @errors = ['sorry, cannot find that question']
+    erb :'/questions/index'
+  end
+end
 
