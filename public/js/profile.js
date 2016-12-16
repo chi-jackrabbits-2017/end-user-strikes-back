053dfb0f16b51a1da254df295f71b307a018923d
Michael  Taylor <email": null>
2016-12-16 12:31:55 -0600

    Make quick edits before lunch

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,9 +1,7 @@
-
 $(function(){
   $(".question-body, .question-answer, .best-answer").hide();
     $(".show-more").on('click', function(event) {
     event.preventDefault();
-    console.log(this)
     $(this).siblings("li, button").show()
   });
 });
